23aedb3fc4cd4c271b1bee4b05fff5f8e40cccb9
William Sutanto <william@jojonomic.com>
2019-05-07 01:01:12 +0700

    add viz, using flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+import json
+from funding_graph import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	graphJSON = {}
+	graphJSON['traveloka'] = get_graph("traveloka.csv",'rgb(93, 164, 214)','rgb(93, 93, 93)',100,3)
+	graphJSON['bukalapak'] = get_graph("bukalapak.csv",'rgb(93, 164, 214)','rgb(93, 93, 93)',10,3)
+	graphJSON['gojek'] = get_graph("gojek.csv",'rgb(93, 164, 214)','rgb(93, 93, 93)',150,4)
+	graphJSON['tokopedia'] = get_graph("tokopedia.csv",'rgb(93, 164, 214)','rgb(93, 93, 93)',150,4)
+	return render_template('index.html',graphJSON=graphJSON)
+	# return render_template('index.html')
---
+++ funding_graph.py
@@ -0,0 +1,53 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+import json
+
+def get_graph(filename,known_color,unknown_color,unknown_fund_size,scaler):
+	dataset = pd.read_csv(filename)
+	known_color = 'rgb(93, 164, 214)'
+	unknown_color = 'rgb(93, 93, 93)'
+	n_funding = len(dataset)
+	unknown_code = -1
+	unknown_fund_size=100
+	scaler = 3
+
+	funding_amount = dataset['Amount'].fillna(unknown_code).tolist()
+	bubble_size = [s/scaler if s!=-1 else s*unknown_fund_size*-1/scaler for s in funding_amount ]
+	bubble_color = [known_color if s!=-1 else unknown_color for s in funding_amount ]
+	bubble_date = dataset['Date'].tolist()
+	bubble_text = dataset['Round'].tolist()
+
+	trace0 = go.Scatter(
+		text=bubble_text,
+		x=bubble_date,
+		y=[0]*n_funding,
+		hoverinfo= 'text',
+		mode='markers',
+		marker=dict(
+			color=bubble_color,
+			size=bubble_size,
+		)
+	)
+	 
+	lay = go.Layout(
+		xaxis=dict(
+			range = [min(bubble_date),max(bubble_date)],
+			showgrid=False,
+			zeroline=False,
+			showline=False
+
+		),
+		yaxis=dict(
+			ticks='',
+	        showgrid=False,
+	        zeroline=False,
+	        showline=False,
+			showticklabels=False
+		),
+		
+	)
+	data=[trace0]
+	graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+	return graphJSON
